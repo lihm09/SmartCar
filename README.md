a0c95d779539b0dc9c9df3a6455c4f47959662b5
everpcpc <everpcpc@live.cn>
2012-10-06 17:31:16 +0800

    修改readme

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 SmartCar
 ========
 
-智能车大赛官网
+#智能车大赛官网
+
+##使用的库
+* django-userena 用户注册登录等
+* django-tracking 统计信息
--- SmartCar/context_processors.py
+++ SmartCar/context_processors.py
@@ -2,7 +2,7 @@
 import datetime
 
 def siteinfo(request):
-    info={'site_name':'第八届智能车竞赛',
+    info={'site_name':'第七届智能车竞赛',
           'year':datetime.datetime.now().year,
           'org':'清华大学工程物理系学生科协'}
     return info
